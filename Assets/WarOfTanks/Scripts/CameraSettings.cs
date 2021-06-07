de0899fa1125040ab587b5820f4c40886fd90ef3
Hikmet <hikmetiskifoglu@gmail.com>
2021-06-07 04:03:13 +0300

    no message

---
+++ Assets/WarOfTanks/Scripts/CameraSettings.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WarOfTanks
+{
+    [CreateAssetMenu(menuName = "War Of Tanks/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] private float _rotationLerpSpeed = 1;
+        public float RotationLerpSpeed { get { return _rotationLerpSpeed; } }
+
+        [Header("Offset")]
+        [SerializeField] private Vector3 _positionOffset;
+        public Vector3 PositionOffset { get { return _positionOffset; } }
+
+        [Header("Lerp")]
+
+        [SerializeField] private float _positionLerp=1;
+        public float PositionLerp { get { return _positionLerp; } }
+
+    }
+}
+
